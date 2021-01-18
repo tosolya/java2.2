e68b575fc46c57507fde049f3420dbb201f3e0b6
Olga Tarasova <bos.olga18@gmail.com>
2021-01-18 19:08:20 +0300

    Внесли изменения

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double regularBonus = 0.9;
+        double regularBonus = 0.3;
         double specialBonus = 0.6;
         double totalBonus = regularBonus + specialBonus;
         System.out.println(totalBonus);
